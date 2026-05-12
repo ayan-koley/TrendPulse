8944d18a757461d282cac8d388470edbecaa779a
Ayan Koley <koleyayan07@gmail.com>
2026-05-12 10:01:27 +0530

    Add index model for exporting all data models and enums

---
+++ server/src/models/index.ts
@@ -0,0 +1,33 @@
+import { usersTable, userRoleEnum, userSubscriptionEnum } from './users.models.ts'
+import { platformsTable, platformNameEnum, apiStatusEnum } from './platforms.models.ts'
+import { trendsTable, countryEnum } from './trends.models.ts'
+import { hashtagsTable, velocityEnum } from './hashtags.models.ts'
+import { trendAnalyticsTable } from './trendAnalytics.models.ts'
+import { platformFetchLogsTable, fetchTypeEnum, fetchStatusEnum } from './platformFetchLogs.models.ts'
+import { savedTrendsTable } from './savedTrends.models.ts'
+import { searchHistoryTable } from './searchHistory.models.ts'
+import { subscriptionsTable, userSubscriptionStatusEnum, userSubscriptionStatus } from './subscriptions.models.ts'
+import { trendHashtags } from './trendHashtags.models.ts'
+
+export {
+    usersTable,
+    userRoleEnum,
+    userSubscriptionEnum,
+    platformsTable,
+    platformNameEnum,
+    apiStatusEnum,
+    trendsTable,
+    countryEnum,
+    hashtagsTable,
+    velocityEnum,
+    trendAnalyticsTable,
+    platformFetchLogsTable,
+    fetchTypeEnum,
+    fetchStatusEnum,
+    savedTrendsTable,
+    searchHistoryTable,
+    subscriptionsTable, 
+    userSubscriptionStatusEnum, 
+    userSubscriptionStatus,
+    trendHashtags
+}
