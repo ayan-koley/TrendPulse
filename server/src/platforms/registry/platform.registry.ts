ee006439d59ddf97220dfd1dca5c015b9457af6a
Ayan Koley <koleyayan07@gmail.com>
2026-05-14 22:43:32 +0530

    Add platforms registry to include youtubePlatform configuration

---
+++ server/src/platforms/registry/platform.registry.ts
@@ -0,0 +1,4 @@
+import { youtubePlatform } from "../configs/youtube.configs.ts";
+import type { PlatformTypes } from "../types/platform.types.ts";
+
+export const platforms: PlatformTypes[] = [ youtubePlatform ];
