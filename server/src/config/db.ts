aed4684f4882b5145055adf568a76c9a2827bc27
Ayan Koley <koleyayan07@gmail.com>
2026-05-16 22:53:25 +0530

    Comment out connectDB function and directly export db instance for simplified database connection handling

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,13 +1,15 @@
 import 'dotenv/config';
 import { drizzle, NodePgDatabase } from 'drizzle-orm/node-postgres';
 
-export const connectDB = async(): Promise<NodePgDatabase> => {
-    try {
-        const db = drizzle(process.env.DATABASE_URL!);
-        console.log("Database is connected ");
-        return db;
-    } catch (e: any) {
-        console.error("Database connection error ::: ", e.message);
-        throw e;
-    }
-}
+// export const connectDB = async(): Promise<NodePgDatabase> => {
+//     try {
+//         const db = drizzle(process.env.DATABASE_URL!);
+//         console.log("Database is connected ");
+//         return db;
+//     } catch (e: any) {
+//         console.error("Database connection error ::: ", e.message);
+//         throw e;
+//     }
+// }
+
+export const db = drizzle(process.env.DATABASE_URL!);
