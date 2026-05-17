5e4893532d347bc926edb76d609eeeae70f896d9
Ayan Koley <koleyayan07@gmail.com>
2026-05-17 15:47:03 +0530

    Refactor calculateTrendScore function to accept an object with engagementCount, velocityScore, and postCount for improved data handling and calculation accuracy

--- server/src/utils/trendScore.utils.ts
+++ server/src/utils/trendScore.utils.ts
@@ -1,3 +1,25 @@
-export function calculateTrendScore(engagement: number): number {
-    return Math.min(100, (engagement / 1_000_000) & 100);
+type TrendScoreInput = {
+  engagementCount: number;
+  velocityScore: number;
+  postCount: number;
+};
+
+export function calculateTrendScore({
+    engagementCount,
+    velocityScore,
+    postCount
+}: TrendScoreInput): number {
+
+    const normalizedEngagement = Math.log10(engagementCount + 1) * 20;
+
+    const normalizedPosts =  Math.log10(postCount + 1) * 15;
+
+    const score =
+        normalizedEngagement +
+        velocityScore * 0.5 +
+        normalizedPosts;
+
+    return Number(
+        Math.min(100, score).toFixed(2)
+    );
 }
