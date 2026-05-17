b7391dd17790db3e717786c17725af2a84d89cb6
Ayan Koley <koleyayan07@gmail.com>
2026-05-17 15:48:04 +0530

    Add calculateEngagementRate function to compute engagement rate based on total views and engagement

--- server/src/utils/engagement.utils.ts
+++ server/src/utils/engagement.utils.ts
@@ -1,3 +1,6 @@
 export function calculateEngagement(views: number, likes: number, comments: number): number {
     return ((views * 1) + (likes * 10) + (comments * 5));
 }
+export function calculateEngagementRate(totalViews: number, engagement: number): number {
+    return totalViews === 0 ? 0 : (engagement / totalViews) * 100
+}  
