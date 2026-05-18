bf9df6a8a6fb794cdc1476afebdadc80f4ab2778
Ayan Koley <koleyayan07@gmail.com>
2026-05-18 19:10:10 +0530

    Remove database connection logic from index.ts and simplify server startup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,6 @@
+import 'dotenv/config';
 import app from "./app.js";
-import { connectDB } from "./config/db.js";
 
-
-// connecting to the database 
-connectDB().then(() => {
-    app.listen(3000, () => {
-        console.log(`Server is running on port 3000`);
-    });
-}).catch((error: any) => {
-    console.error("Database connection error ::: ", error.message);
+app.listen(3000, () => {
+    console.log(`Server is running on port 3000`);
 });
-
