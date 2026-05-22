abebfdf07709c31a87e42bae003a817bd15bd44c
Ayan Koley <koleyayan07@gmail.com>
2026-05-22 19:41:13 +0530

    Refactor getDashboardOverviewService to include error handling and return structured dashboard data

--- server/src/services/dashboard/getDashboardOverview.service.ts
+++ server/src/services/dashboard/getDashboardOverview.service.ts
@@ -1,3 +1,6 @@
+import type { TrendingTopics } from "../normalizers/youtube.normalizer.ts";
+import { getDashboardOverviewStats, getSparklineData, getTopTrendsForDashboard } from "../repositories/dashboard.repositories.ts";
+
 export const getDashboardOverviewService =
   async () => {
 
@@ -5,7 +8,16 @@ export const getDashboardOverviewService =
         // TODO:
         // Fetch top active trends
         // Example:
-        // top 20 trends ordered by trend_score
+        // top 20 trends ordered by trend_score / rank_position
+        const topTrends = await getTopTrendsForDashboard();
+        if(!topTrends) {
+            throw new Error("Unable to fetch top trends for dashboard overview");
+        }
+
+        for(const trend of topTrends) {
+            const sparklineData: number[] = await getSparklineData(trend.id);
+            (trend as any).sparkline = sparklineData;
+        }
 
 
         // TODO:
@@ -15,6 +27,7 @@ export const getDashboardOverviewService =
         // active trends
         // top platform
         // fastest growing trend
+        const dashboardStats = await getDashboardOverviewStats();
 
 
         // TODO:
@@ -24,25 +37,19 @@ export const getDashboardOverviewService =
 
 
         // TODO:
-        // Fetch sparkline data
-        // Example:
-        // last 5-10 trend_score snapshots
-        // from trend_analytics table
-
-
-        // TODO:
-        // Merge trend data with sparkline arrays
-        // so frontend can directly render cards
-
-
-        // TODO:
         // Format frontend-ready response
 
 
         // TODO:
         // Return final dashboard object
+        return {
+            overview: {
+              ...dashboardStats
+            },
+            topTrends
+        };
     } catch (error: any) {
-        
+        console.error("Error in getDashboardOverviewService:", error);
     }
 
 };
