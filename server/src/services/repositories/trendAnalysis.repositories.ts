04056d2da77dc9f3ea2a25b02af81de463d2ba1c
Ayan Koley <koleyayan07@gmail.com>
2026-05-22 19:42:07 +0530

    Add trend analysis repository with insertTrendAnalytics function and type definition

---
+++ server/src/services/repositories/trendAnalysis.repositories.ts
@@ -0,0 +1,41 @@
+import { eq } from "drizzle-orm";
+import { db } from "../../config/db.ts";
+import { trendAnalyticsTable } from "../../models/trendAnalytics.models.ts";
+import { platformsTable } from "../../models/platforms.models.ts";
+import { trendsTable } from "../../models/trends.models.ts";
+
+export type TrendAnalysis = {
+    trend_id: string,
+    platform_id: string,
+    time_bucket: Date,
+    hourly_growth?: number,
+    daily_growth?: number,
+    engagement_rate: number,
+    viral_probability?: number,
+    post_count: number,
+    top_countries: string[],
+    created_at: Date,
+    trend_score: number
+} 
+
+const insertTrendAnalytics = async(trend: TrendAnalysis) => {
+    try {
+        const isValidPlatformId = await db.select({id: platformsTable}).from(platformsTable).where(eq(platformsTable.id, trend.platform_id));
+        if(isValidPlatformId?.length === 0) {
+            throw new Error("Invalid platform id");
+        }
+
+        const isValidTrendId = await db.select({id: trendsTable.id}).from(trendsTable).where(eq(trendsTable.id, trend.trend_id));
+        if(isValidTrendId?.length === 0) {
+            throw new Error("Invalid trend id");
+        }
+
+        return await db.insert(trendAnalyticsTable).values(trend);
+    } catch (error: any) {
+        throw new Error("ERROR on inserting data into trendAnalytics ", error.message);
+    }
+}
+
+export {
+    insertTrendAnalytics
+}
