e199c98a6c9a4e8efe3680d7e81cabbb7b95deed
Ayan Koley <koleyayan07@gmail.com>
2026-05-25 22:56:26 +0530

    FIX: Noramized the hashtag for the trending topic

--- server/src/services/trend/groupVideosByHashtag.ts
+++ server/src/services/trend/groupVideosByHashtag.ts
@@ -1,14 +1,22 @@
 import type { NormalizedVideo } from "../normalizers/youtube.normalizer.ts";
 
+export const HASHTAG_BLACKLIST = new Set([
+    'viral', 'trending', 'foryou', 'fyp', 'shorts', 
+    'video', 'youtube', 'instagram', 'explore', 'reels'
+]);
+
 export const groupVideosByHashtags = (videos: NormalizedVideo[]): Map<string, NormalizedVideo[]> => {
     const hashMap: Map<string, NormalizedVideo[]> = new Map();
 
     for(const video of videos) {
         for(const tag of video.hashtags) {
-            if(!hashMap.has(tag)) {
-                hashMap.set(tag, []);
+            // Normalized hashtag
+            const cleanedTag = tag.toLowerCase().replace(/[^a-z0-9]/g, "");
+            if(!cleanedTag || HASHTAG_BLACKLIST.has(cleanedTag)) continue;
+            if(!hashMap.has(cleanedTag)) {
+                hashMap.set(cleanedTag, []);
             }
-            hashMap.get(tag)?.push(video);
+            hashMap.get(cleanedTag)?.push(video);
         }
     } 
     return hashMap;
