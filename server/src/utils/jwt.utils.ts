5e84692769b4d4b7d781558f0e13415a6b434405
Ayan Koley <koleyayan07@gmail.com>
2026-05-26 20:06:42 +0530

    ADD: implement JWT utility functions for access and refresh token generation

---
+++ server/src/utils/jwt.utils.ts
@@ -0,0 +1,21 @@
+import jwt, { type Algorithm } from 'jsonwebtoken';
+
+interface TokenPayload {
+    userId: string;
+    email: string;
+    role: string;
+}
+
+export const generateAccessToken = (payload: TokenPayload) => {
+    return jwt.sign(payload, process.env.JWT_ACCESS_SECRET!, { 
+        algorithm: ( process.env.JWT_ALGORITHM || "HS256" )as Algorithm,
+        expiresIn: (process.env.JWT_ACCESS_EXPIRES_IN || "15m") as any ,
+    })
+}
+
+export const generateRefreshToken = (payload: TokenPayload) => {
+    return jwt.sign(payload, process.env.JWT_REFRESH_SECRET!, { 
+        algorithm: ( process.env.JWT_ALGORITHM || "HS256" )as Algorithm,
+        expiresIn: (process.env.JWT_REFRESH_EXPIRES_IN || "7d") as any ,
+    })
+}
