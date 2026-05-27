2bf85b43a59afd8adb2132f2d2df03e0f6aaa642
Ayan Koley <koleyayan07@gmail.com>
2026-05-27 11:04:13 +0530

    ADD: implement /me route to retrieve current user information

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createUser, loginUser, logoutUser, refreshRefreshToken } from '../controllers/user.controllers.ts';
+import { createUser, getCurrentUser, loginUser, logoutUser, refreshRefreshToken } from '../controllers/user.controllers.ts';
 import { verifyToken } from '../middlewares/auth.middlewares.ts';
 const router = Router();
 
@@ -7,5 +7,6 @@ router.route('/create').post(createUser);
 router.route('/login').post(loginUser);
 router.route('/logout').post(verifyToken, logoutUser);
 router.route('/refresh-token').post(refreshRefreshToken);
+router.route('/me').get(verifyToken, getCurrentUser);
 
 export default router;
