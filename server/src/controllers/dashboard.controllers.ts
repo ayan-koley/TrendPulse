ff727c558c760d23c06b71a268ffa035e717642e
Ayan Koley <koleyayan07@gmail.com>
2026-05-30 08:13:33 +0530

    ADD: enhance getDashboardOverview to return a structured API response

--- server/src/controllers/dashboard.controllers.ts
+++ server/src/controllers/dashboard.controllers.ts
@@ -1,13 +1,14 @@
 import type { Request, Response, NextFunction } from "express";
 import { asyncHandler } from "../utils/asyncHandler.ts";
 import { getDashboardOverviewService } from "../services/dashboard/getDashboardOverview.service.ts";
+import { ApiResponse } from "../utils/ApiResponse.ts";
 
 
 const getDashboardOverview = asyncHandler(async(req: Request, res: Response, next: NextFunction) => {
     const response = await getDashboardOverviewService();
 
     return res.status(200).json(
-        response
+        ApiResponse.success(response, "Dashboard overview fetched successfully")
     );
 })
 
