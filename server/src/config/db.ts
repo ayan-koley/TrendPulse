9b93b693a904de42598bb309804146a486b13941
Ayan Koley <koleyayan07@gmail.com>
2026-06-08 20:19:00 +0530

    UPDATE: refactor database connection setup to use connection pool and improved error handling

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,15 +1,26 @@
 import 'dotenv/config';
 import { drizzle, NodePgDatabase } from 'drizzle-orm/node-postgres';
+import pg from 'pg';
 
-// export const connectDB = async(): Promise<NodePgDatabase> => {
-//     try {
-//         const db = drizzle(process.env.DATABASE_URL!);
-//         console.log("Database is connected ");
-//         return db;
-//     } catch (e: any) {
-//         console.error("Database connection error ::: ", e.message);
-//         throw e;
-//     }
-// }
+if(!process.env.DATABASE_URL) {
+    throw new Error("❌ DATABASE_URL missing from environment variables!")
+}
 
-export const db = drizzle(process.env.DATABASE_URL!);
+const pool = new pg.Pool(
+    {
+        connectionString: process.env.DATABASE_URL,
+        max: 20,
+        idleTimeoutMillis: 30000,
+        connectionTimeoutMillis: 2000
+    }
+)
+
+pool.on('connect', () => {
+    console.log('🐘 PostgreSQL connection pool initialized successfully.');
+})
+
+pool.on('error', (err) => {
+  console.error('⚠️ Unexpected error on idle PostgreSQL client:', err);
+});
+
+export const db = drizzle(pool);
